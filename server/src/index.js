5eb832dac3908339a0e28871fc346e8103b4ac68
FESTUS ADEKUNLE <olusolaadek@hotmail.com>
2026-08-06 21:06:33 -0600

    fix: update graphql version and enhance server initialization
    - Downgraded graphql dependency from ^16.14.2 to ^16.11.0 for compatibility.
    - Upgraded dotenv to ^17.4.2 and nodemon to ^3.1.14 for improved development experience.
    - Implemented Apollo Server initialization in index.js with standalone server setup.

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,17 @@
 //TODO
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const typeDefs = require('./schema');
+
+async function startApolloServer() {
+    const server = new ApolloServer({
+        typeDefs: typeDefs.typeDefs,
+    });
+
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+    console.log(`Server is running at ${url}`);
+}
+
+startApolloServer();

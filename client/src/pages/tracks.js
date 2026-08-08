e252442f773db34e806cdceb2be77e6a1090b168
FESTUS ADEKUNLE <olusolaadek@hotmail.com>
2026-08-08 14:56:15 -0600

    Part completed: update Apollo Client and GraphQL dependencies; implement tracks query with mock data

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
+
+/** * GraphQL query to get the tracks for the homepage grid
+ */
+const TRACKS = gql`
+  query GetTrackList {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
--- server/src/index.js
+++ server/src/index.js
@@ -3,37 +3,35 @@ const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const { addMocksToSchema } = require('@graphql-tools/mock');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-
+const { randomInt } = require('crypto');
 const typeDefs = require('./schema');
-
+let _id = 1;
 const mocks = {
     Query: () => ({
-        tracksForHome: () => [...new Array(8)],
+        tracksForHome: () =>
+            Array.from({ length: 8 }, (_, index) => ({
+                id: `track_${index + 1}`,
+                title: `Astro Kitty, Space Explorer ${index + 1}`,
+            })),
     }),
     Track: () => ({
-        id: () => "track_01",
-        title: () => "Astro Kitty, Space Explorer",
-        author: () => {
-            return {
-                name: "Grumpy Cat",
-                photo:
-                    "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
-            };
-        },
+        author: () => ({
+            name: "Grumpy Cat",
+            photo:
+                "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
+        }),
         thumbnail: () =>
             "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
         length: () => 1210,
-        modulesCount: () => 8,
+        modulesCount: () => randomInt(1, 10),
     }),
 };
 
 async function startApolloServer() {
     const server = new ApolloServer({
         schema: addMocksToSchema({
-            schema: addMocksToSchema({
-                schema: makeExecutableSchema({ typeDefs: typeDefs.typeDefs }),
-                mocks: mocks,
-            }),
+            schema: makeExecutableSchema({ typeDefs: typeDefs.typeDefs }),
+            mocks,
         }),
     });
 
